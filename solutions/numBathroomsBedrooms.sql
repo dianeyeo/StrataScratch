62b5f81100d5b9342912214a3aefaa56ce770931
DianeYeo <ystdiane26@gmail.com>
2021-10-15 20:09:31 +0800

    new: Solution for #9622
    Solution for Number of Bathrooms and Bedrooms
    Difficulty: Easy

---
+++ solutions/numBathroomsBedrooms.sql
@@ -0,0 +1,35 @@
+/*
+Number of Bathrooms and Bedrooms
+https://platform.stratascratch.com/coding/9622-number-of-bathrooms-and-bedrooms?python=
+
+Difficulty: Easy
+
+-- Find the average number of bathrooms and bedrooms for each city’s property types. Output the result along with the city name and the property type.
+
+Tables:
+airbnb_search_details
+    id                      int
+    price                   float
+    property_type           varchar
+    room_type               varchar
+    amenities               varchar
+    accommodates            int
+    bathrooms               int
+    bed_type                varchar
+    cancellation_policy     varchar
+    cleaning_fee            bool
+    city                    varchar
+    host_identity_verified  varchar
+    host_response_rate      varchar
+    host_since              datetime
+    neighbourhood           varchar
+    number_of_reviews       int
+    review_scores_rating    float
+    zipcode                 int
+    bedrooms                int
+    beds                    int
+*/
+
+SELECT city, property_type, AVG(bathrooms) AS avgNumBathroom, AVG(bedrooms) AS avgNumBedroom
+FROM airbnb_search_details
+GROUP BY city, property_type;
