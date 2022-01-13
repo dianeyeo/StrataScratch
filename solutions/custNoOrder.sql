1183e093b59d0373eddcf4b5834b17ce2e6d684c
Diane Yeo <ystdiane26@gmail.com>
2022-01-13 22:41:30 +0800

    new: Solution for #9896
    Customers without Orders
    Difficulty: Medium

---
+++ solutions/custNoOrder.sql
@@ -0,0 +1,28 @@
+/*
+Customers without Orders
+https://platform.stratascratch.com/coding/9896-customers-without-orders?python=
+
+Difficulty: Medium
+
+Tables:
+customers
+    id                  int
+    first_name          varchar
+    last_name           varchar
+    city                varchar
+    address             varchar
+    phone_number        int
+
+orders
+    id                  int
+    cust_id             int
+    order_date          datetime
+    order_details       varchar
+    total_order_cost    int
+*/
+
+SELECT first_name AS customer_name
+FROM customers
+WHERE id NOT IN
+    (SELECT cust_id
+    FROM orders);
