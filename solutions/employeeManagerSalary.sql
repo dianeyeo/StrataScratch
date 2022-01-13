9eb6a3417078aca7b39051ad6c087841125b96b9
Diane Yeo <ystdiane26@gmail.com>
2022-01-13 22:19:52 +0800

    new: Solution for #9894
    Employee and Manager Salary
    Difficulty: Medium

---
+++ solutions/employeeManagerSalary.sql
@@ -0,0 +1,30 @@
+/*
+Employee and Manager Salaries
+https://platform.stratascratch.com/coding/9894-employee-and-manager-salaries?python=
+
+Difficulty: Medium
+
+Tables:
+employee
+    id                  int
+    first_name          varchar
+    last_name           varchar
+    age                 int
+    sex                 varchar
+    employee_title      varchar
+    department          varchar
+    salary              int
+    target              int
+    bonus               int
+    email               varchar
+    city                varchar
+    address             varchar
+    manager_id          int
+*/
+
+SELECT n.first_name AS employeeName, n.salary AS employeeSalary
+FROM employee AS n
+JOIN employee AS m
+ON n.manager_id = m.id
+-- if manager is also the staff
+WHERE n.salary > m.salary;
