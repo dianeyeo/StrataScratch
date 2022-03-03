fdd5dfcc6d92ce8152c20320ff86be960951e2e5
Diane Yeo <ystdiane26@gmail.com>
2022-03-03 23:47:56 +0800

    edit: question new lines

--- solutions/totalCostOrders.sql
+++ solutions/totalCostOrders.sql
@@ -4,7 +4,8 @@ https://platform.stratascratch.com/coding/10183-total-cost-of-orders?python=
 
 Difficulty: Easy
 
--- Find the total cost of each customer's orders. Output customer's id, first name, and the total order cost. Order records by customer's first name alphabetically.
+-- Find the total cost of each customer's orders. Output customer's id, first name, and the total order cost. 
+-- Order records by customer's first name alphabetically.
 
 Tables:
 customers
