32b29c8a4a6497e6fe83d9a20c2ebf8b517ea084
Diane Yeo <ystdiane26@gmail.com>
2022-03-03 23:46:50 +0800

    edit: questions new lines

--- solutions/customerDetails.sql
+++ solutions/customerDetails.sql
@@ -6,7 +6,9 @@ https://platform.stratascratch.com/coding/9891-customer-details?python=
 Difficulty: Easy
 
 -- Find the details of each customer regardless of whether the customer made an order. Output the customer's first name, last name, and the city along with the order details.
-Your output should be listing the customer's orders not necessarily listing the customers. This means that you may have duplicate rows in your results due to a customer ordering several of the same items. Sort records based on the customer's first name and the order details in ascending order.
+-- Your output should be listing the customer's orders not necessarily listing the customers. 
+-- This means that you may have duplicate rows in your results due to a customer ordering several of the same items. 
+-- Sort records based on the customer's first name and the order details in ascending order.
 
 Tables:
 customers
