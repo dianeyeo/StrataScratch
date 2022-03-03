34f7c7fe6f4f7ccf5e7d42a45294bf05be35f530
Diane Yeo <ystdiane26@gmail.com>
2022-03-03 23:45:11 +0800

    edit: add in tables

--- solutions/favouriteCustomer.sql
+++ solutions/favouriteCustomer.sql
@@ -8,6 +8,22 @@ Difficulty: Medium
 -- A customer is considered as a favorite if he or she has placed more than 3 orders and with the total cost of orders more than $100.
 
 -- Output the customer's first name, city, number of orders, and total cost of orders.
+
+Tables:
+customers
+    id                  int
+    first_name          varchar
+    last_name           varchar
+    city                varchar
+    address             varchar
+    phone_number        int
+
+orders
+    id                  int
+    cust_id             int
+    order_date          datetime
+    order_details       varchar
+    total_order_cost    int
 */
 
 SELECT c.first_name, c.city, COUNT(o.id) AS order_count, SUM(o.total_order_cost) AS total_cost
