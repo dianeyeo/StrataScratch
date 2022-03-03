b91e2efd31234dfe13ff126ac921de17b3ee8d48
Diane Yeo <ystdiane26@gmail.com>
2022-03-03 23:38:18 +0800

    edit: add in question

--- solutions/workersHighestSalaries.sql
+++ solutions/workersHighestSalaries.sql
@@ -4,6 +4,9 @@ https://platform.stratascratch.com/coding/10353-workers-with-the-highest-salarie
 
 Difficulty: Medium
 
+-- Find the titles of workers that earn the highest salary. 
+-- Output the highest-paid title or multiple titles that share the highest salary.
+
 Tables:
 worker
     worker_id       int
