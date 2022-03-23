944c144d212a2f38082602cb08bf43467ee2b152
Diane Yeo <ystdiane26@gmail.com>
2022-03-23 18:11:41 +0800

    new: Solution for #10324

---
+++ solutions/distanceTraveled.sql
@@ -0,0 +1,18 @@
+/*
+Distances Traveled
+https://platform.stratascratch.com/coding/10324-distances-traveled?python=
+
+Difficulty: Medium
+
+-- Find the top 10 users that have traveled the greatest distance. 
+-- Output their id, name and a total distance traveled.
+*/
+
+SELECT u.id, u.name, SUM(l.distance) AS total_distance
+FROM lyft_rides_log AS l
+FULL JOIN lyft_users AS u
+ON l.user_id = u.id
+GROUP BY u.id, u.name
+HAVING SUM(l.distance) IS NOT NULL
+ORDER BY total_distance DESC
+LIMIT 10;
