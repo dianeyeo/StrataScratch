393ac943ed2a8abde6333e0e93d7ccf59e917ba2
Diane Yeo <ystdiane26@gmail.com>
2022-08-25 22:43:52 +0800

    new: Solution for #9996

---
+++ solutions/totalAvgStreams.sql
@@ -0,0 +1,22 @@
+/*
+https://platform.stratascratch.com/coding/9996-find-the-average-number-of-streams-across-all-songs?code_type=1
+
+Difficulty: Easy
+
+-- Find the average number of streams across all songs.
+
+Tables:
+spotify_worldwide_daily_song_ranking
+    id              int
+    position        int
+    trackname       varchar
+    artist          varchar
+    streams         int
+    url             varchar
+    date            datetime
+    region          varchar
+*/
+*/
+
+SELECT AVG(streams) AS avg_streams 
+FROM spotify_worldwide_daily_song_ranking;
